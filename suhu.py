42aa1ccfb400479defa0ae2c159cb672a6f1c024
lailysmn <111669738+lailysmn@users.noreply.github.com>
2023-03-28 18:52:07 +0700

    second commit

---
+++ suhu.py
@@ -0,0 +1,4 @@
+celcius = float(input('Masukan suhu dalam derajat Celcius :'))
+fahrenheit = (9/5 * celcius) + 32
+print("Nilai Celcius: ", celcius, "C")
+print("Nilai dalan fahrenheit :", fahrenheit, "f")
